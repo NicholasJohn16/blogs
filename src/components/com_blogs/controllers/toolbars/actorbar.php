d374070ff4b01d1b652ac89ac18998803a72b989
Nick Swinford <nicholasjohn16@gmail.com>
2017-11-04 04:59:31 -0400

    update to Anahita 4.3

--- src/components/com_blogs/controllers/toolbars/actorbar.php
+++ src/components/com_blogs/controllers/toolbars/actorbar.php
@@ -24,10 +24,10 @@ class ComBlogsControllerToolbarActorbar extends ComMediumControllerToolbarActorb
         $layout = pick($this->getController()->layout, 'default');
         $name = $this->getController()->getIdentifier()->name;
 
-        $this->setTitle(JText::sprintf('COM-BLOGS-HEADER-BLOGS', $actor->name));
+        $this->setTitle(AnTranslator::sprintf('COM-BLOGS-HEADER-BLOGS', $actor->name));
 
         $this->addNavigation('blogs',
-                JText::_('COM-BLOGS-NAV-BLOGS'),
+                AnTranslator::_('COM-BLOGS-NAV-BLOGS'),
                 array(
                     'option' => 'com_blogs',
                     'view' => 'blogs',
--- src/components/com_blogs/domains/entities/component.php
+++ src/components/com_blogs/domains/entities/component.php
@@ -15,16 +15,16 @@ class ComBlogsDomainEntityComponent extends ComMediumDomainEntityComponent
     {
         if ($mode == 'profile') {
             $gadgets->insert('blogs', array(
-                    'title' => JText::_('COM-BLOGS-GADGET-ACTOR-PROFILE'),
+                    'title' => AnTranslator::_('COM-BLOGS-GADGET-ACTOR-PROFILE'),
                     'url' => 'option=com_blogs&view=blogs&layout=gadget&oid='.$actor->id,
-                    'action' => JText::_('LIB-AN-GADGET-VIEW-ALL'),
+                    'action' => AnTranslator::_('LIB-AN-GADGET-VIEW-ALL'),
                     'action_url' => 'option=com_blogs&view=blogs&oid='.$actor->id,
             ));
         } else {
             $gadgets->insert('blogs', array(
-                    'title' => JText::_('COM-BLOGS-GADGET-ACTOR-DASHBOARD'),
+                    'title' => AnTranslator::_('COM-BLOGS-GADGET-ACTOR-DASHBOARD'),
                     'url' => 'option=com_blogs&view=blogs&layout=gadget&filter=leaders',
-                    'action' => JText::_('LIB-AN-GADGET-VIEW-ALL'),
+                    'action' => AnTranslator::_('LIB-AN-GADGET-VIEW-ALL'),
                     'action_url' => 'option=com_blogs&view=blogs&filter=leaders',
             ));
         }
@@ -37,8 +37,8 @@ class ComBlogsDomainEntityComponent extends ComMediumDomainEntityComponent
     {
         if ($actor->authorize('action', 'com_blogs:blog:add')) {
             $composers->insert('photos-composer', array(
-                    'title' => JText::_('COM-BLOGS-COMPOSER-BLOG'),
-                    'placeholder' => JText::_('COM-BLOGS-BLOG-ADD'),
+                    'title' => AnTranslator::_('COM-BLOGS-COMPOSER-BLOG'),
+                    'placeholder' => AnTranslator::_('COM-BLOGS-BLOG-ADD'),
                     'url' => 'option=com_blogs&view=blog&layout=composer&oid='.$actor->id,
             ));
         }
@@ -50,7 +50,7 @@ class ComBlogsDomainEntityComponent extends ComMediumDomainEntityComponent
     protected function _setMenuLinks($actor, $menuItems)
     {
         $menuItems->insert('blogs-blogs', array(
-            'title' => JText::_('COM-BLOGS-MENU-ITEM-BLOGS'),
+            'title' => AnTranslator::_('COM-BLOGS-MENU-ITEM-BLOGS'),
             'url' => 'option=com_blogs&view=blogs&oid='.$actor->uniqueAlias,
         ));
     }
