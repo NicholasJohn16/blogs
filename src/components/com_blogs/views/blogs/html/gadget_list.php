73571d83ac24e6e6b170d94c0a91c0cb581ac927
Nick Swinford <nicholasjohn16@gmail.com>
2021-10-17 00:03:39 -0400

    fix filename for gadget_list

--- src/components/com_blogs/views/blogs/html/gadget_list.php
+++ src/components/com_blogs/views/blogs/html/gadget_list.php
@@ -1,9 +1,9 @@
-<?php defined('KOOWA') or die('Restricted access');?>	
+<?php defined('KOOWA') or die('Restricted access');?>
 	
 <?php if (count($blogs)) :?>
 	<?php foreach ($blogs as $blog) : ?>
 	<?= @view('blog')->layout('list')->blog($blog)->filter($filter) ?>
 	<?php endforeach; ?>
 <?php else: ?>
-<?= @message(@text('COM-BLOGS-BLOGS-EMPTY-LIST-MESSAGE')) ?>
+	<?= @message(@text('COM-BLOGS-BLOGS-EMPTY-LIST-MESSAGE')) ?>
 <?php endif; ?>
